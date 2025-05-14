7679a6b7148611b55cf54812bfd2ec87148246e2
mingwangrrc <mingwang@academic.rrc.ca>
2025-05-14 13:45:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Post from './components/Post.jsx';
+
+
+function App() { 
+  return (<main>
+    <Post author ="Maximilian" body="React.js is awesome" />
+    <Post author ="Manuel" body="Check out the full course"/>
+
+    
+  </main>
+);
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,19 @@
+// const name = ['Maximilian','Manuel'];
+import classes from './Post.module.css'
+
+
+function Post(props){
+    // const chosenName = Math.random()>0.5 ? name[0]:name[1];
+    // props.author
+    // props.body
+    return(
+            // function add(a,b){
+            //     return a+b;
+            // }
+        <div className={classes.post}>
+        <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
+        </div>
+    );
+}
+    export default Post;
