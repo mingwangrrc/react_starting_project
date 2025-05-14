e52198b6dd2041e9d7f1a6738b8edb2e2bcf0e41
mingwangrrc <mingwang@academic.rrc.ca>
2025-05-14 15:20:55 -0500

    Preparing the App For State Management

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,14 +1,17 @@
 import Post from './Post.jsx'; 
+import NewPost from './NewPost.module.jsx';
 import classes from './Postlist.module.css';
 
 function PostsList(){
     return(
-        <ul className = {classes.posts}>
-            <Post author ="Maximilian" body="React.js is awesome" />
-            <Post author ="Manuel" body="Check out the full course"/>
-        </ul>
+        <>
+            <NewPost />
+            <ul className = {classes.posts}>
+                <Post author ="Maximilian" body="React.js is awesome" />
+                <Post author ="Manuel" body="Check out the full course"/>
+            </ul>
+        </>
     );
-
 }
 
 export default PostsList;
