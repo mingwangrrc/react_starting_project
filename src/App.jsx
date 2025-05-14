0f9a0b8facb531877066f5551535b0483b1eceae
mingwangrrc <mingwang@academic.rrc.ca>
2025-05-14 14:46:25 -0500

    git commit test3

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Post from './components/Post.jsx';
 
 function App() { 
   return (<main>
-    <Post author ="Maximilian" body="React.js is awesome test2" />
-    <Post author ="Manuel" body="Check out the full course test2"/>
+    <Post author ="Maximilian" body="React.js is awesome test3" />
+    <Post author ="Manuel" body="Check out the full course test3"/>
 
-    
   </main>
 );
 }
