9f43a6d8f4d9b94a96e50179a2649b63d59ff099
mingwangrrc <mingwang@academic.rrc.ca>
2025-05-22 11:00:35 -0500

    State & Conditional Content

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -6,23 +6,43 @@ import classes from './Postlist.module.css';
 import Modal from './Modal.jsx';
 
 function PostList(){
+    const [modalIsVisible,setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler(){
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event){
         setEnteredBody(event.target.value);
     }   
+
     function authorChangeHandler(event){
         setEnteredAuthor(event.target.value);
     }
+
+    // let modalContent;
+    // if(modalIsVisible){
+    //     modalContent = (
+    //             <Modal onClose={hideModalHandler}>
+    //                 <NewPost 
+    //                     onBodyChange={bodyChangeHandler} 
+    //                     onAuthorChange={authorChangeHandler} 
+    //                 />
+    //             </Modal>
+    //             );
+    // }
     return(
         <>
-            <Modal>
-                <NewPost 
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler} 
-                />
-             </Modal>
+            {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
+                   <NewPost 
+                        onBodyChange={bodyChangeHandler} 
+                        onAuthorChange={authorChangeHandler} 
+                     />
+                 </Modal>
+            )}
             <ul className = {classes.posts}>
                 <Post author ={enteredAuthor} body={enteredBody} />
                 <Post author ="Manuel" body="This is a test post" />
